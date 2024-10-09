9790fb39f2b44b43d0dcee05741f49021d258ba6
viet003 <tansinhvien03@gmail.com>
2024-10-09 16:24:10 +0700

    update AuthUI

---
+++ app/components/AuthSocialButton.tsx
@@ -0,0 +1,34 @@
+import { IconType } from "react-icons";
+
+interface AuthSocialButtonProps {
+    icon: IconType;
+    onClick: () => void;
+}
+
+
+
+const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
+    icon: Icon,
+    onClick,
+}) => {
+    return (
+        <button 
+        type="button"
+        onClick={onClick}
+        className="
+        inline-flex
+        w-full
+        justify-center
+        rounded-md
+        bg-white
+        px-4
+        py-2
+        text-gray-500
+        shadow-sm"
+        >
+        <Icon />
+        </button>
+    )
+}
+
+export default AuthSocialButton;
